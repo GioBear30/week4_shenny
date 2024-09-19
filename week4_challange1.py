25386cce4c028ed9b57f49e72eb9ad9b97be3e9f
Aguayo <gjaguayo@cps.k12.il.us>
2024-09-19 12:36:44 -0500

    week4_challange1

---
+++ week4_challange1.py
@@ -0,0 +1,16 @@
+#################################Extracting Sub-Strings###################################
+# Extracting Sub-Strings Practice #1
+# Extract the first word of the following sentence using slicing, and display it on the screen:
+word = "Controlling complexity is the essence of programming"
+print(word[0:11])
+# Hint: "Controlling" is 11 characters long.
+
+# Extracting Sub-Strings Practice #2
+# Take every third character starting from the ninth to the end of the sentence, and print the result.
+word2 = "Never trust a computer you can't throw out a window"
+print(word2[8::3])
+
+# Extracting Sub-Strings Practice #3
+# Reverses the position of all the characters in the following sentence and displays the result on the screen.\
+word3= "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
+print(word3[::-1])
